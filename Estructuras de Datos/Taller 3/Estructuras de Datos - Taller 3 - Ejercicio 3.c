b03701e9d3139ed397a0c251200f1274353b2740
SergioMmCc <sergio.moncada1@utp.edu.co>
2023-10-08 00:08:16 -0500

    Taller 3 actualizado

---
+++ Estructuras de Datos/Taller 3/Estructuras de Datos - Taller 3 - Ejercicio 3.c
@@ -0,0 +1,189 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+# define TRUE 1
+# define FALSE 0
+
+struct node
+{
+    int key ;
+    struct node * next ;
+    struct node * prev ;
+};
+
+struct node *insertElementInCircularDoublyLinkedList ( struct node *tail, int element )
+{
+    struct node * newNode ;
+    newNode = (struct node *) malloc (sizeof (struct node));
+    newNode -> key = element ;
+    if( tail == NULL )
+    {
+        newNode -> next = newNode ;
+        newNode -> prev = newNode ;
+        tail = newNode ;
+    }
+    else
+    {
+        newNode -> next = tail -> next ;
+        newNode ->next -> prev = newNode ;
+        newNode -> prev = tail ;
+        tail -> next = newNode ;
+        tail = newNode ;
+    }
+    return tail ;
+}
+
+struct node *deleteFirstNodeInCircularDoublyLinkedList ( struct node * tail )
+{
+    struct node * firstNode ;
+    if( tail == NULL )
+        printf ("The circular doubly linked list is empty.\n");
+    else
+    {
+        if( tail == tail -> next )
+        {
+            free ( tail ) ;
+            tail = NULL ;
+        }
+        else
+        {
+            firstNode = tail -> next ;
+            tail -> next = firstNode -> next ;
+            tail ->next -> prev = tail ;
+            free ( firstNode );
+        }
+    }
+    return tail ;
+}
+
+void printFromFirstToLastCircularDoublyLinkedList ( struct node * tail )
+{
+    struct node * actualNode ;
+    if( tail == NULL )
+        printf ("NULL \n");
+    else
+    {
+        actualNode = tail -> next ;
+        while ( actualNode != tail )
+        {
+            printf ("%d -> ", actualNode -> key ) ;
+            actualNode = actualNode -> next ;
+        }
+        printf ("%d ...\n", tail -> key );
+    }
+}
+
+void printFromLastToFirstCircularDoublyLinkedList (struct node * tail)
+{
+    struct node * actualNode ;
+    if( tail == NULL )
+        printf ("NULL \n");
+    else
+    {
+        actualNode = tail ;
+        while ( actualNode != tail -> next )
+        {
+            printf ("%d -> ", actualNode -> key ) ;
+            actualNode = actualNode -> prev ;
+        }
+        printf ("%d ...\n", tail ->next -> key );
+    }
+}
+
+struct node *deleteElementOfCircularDoublyLinkedList (struct node *tail, int element)
+{
+    struct node * actualNode ;
+    int flag = FALSE ;
+    if (tail == NULL)
+        printf (" The circular doubly linked list is empty .\n");
+    else
+    {
+        actualNode = tail -> next ;
+        while (( actualNode != tail ) && ( flag == FALSE ))
+        {
+            if( actualNode -> key == element )
+            {
+            flag = TRUE ;
+            break ;
+            }
+            else
+            actualNode = actualNode -> next ;
+        }
+        if( flag == FALSE )
+        {
+            if(tail -> key == element )
+            {
+                flag = TRUE ;
+                actualNode = tail ;
+            }
+        }
+        if( tail == tail -> next )
+        {
+            if( flag == TRUE )
+            {
+                free ( tail ) ;
+                tail = NULL ;
+            }
+            else
+                printf (" The %d is not in the circular doubly linked list .\n", element );
+        }
+        else
+        {
+            if( flag == FALSE )
+                printf (" The %d is not in the circular doubly linked list .\n", element );
+            else
+            {
+                actualNode ->next -> prev = actualNode -> prev ;
+                actualNode ->prev -> next = actualNode -> next ;
+                if( actualNode == tail )
+                    tail = tail -> prev ;
+                free ( actualNode );
+            }
+        }
+    }
+    return tail ;
+}
+
+int main() {
+    int n, k, nIndex, kIndex, moves;
+    struct node *tail;
+    
+    while (scanf ("%d %d", &n, &k) && (n != 0)) {
+        tail = NULL;
+        moves = 0;
+        
+        for (nIndex = 1; nIndex <= n; nIndex++)
+            tail = insertElementInCircularDoublyLinkedList (tail, nIndex);
+        
+        tail = tail->next;
+        
+        while (tail != tail->next) {
+            if (moves == 0) {
+                
+                if (k == 1)
+                    tail = tail->prev;
+                else {
+                    for (kIndex = 1; kIndex < k-1; kIndex++)
+                    tail = tail->next;
+                }
+                
+                tail = deleteFirstNodeInCircularDoublyLinkedList (tail);
+                tail = tail->next;
+                moves = 1;
+            }
+            else if (moves == 1) {
+                for (kIndex = 1; kIndex <= k; kIndex++)
+                    tail = tail->prev;
+
+                tail = deleteFirstNodeInCircularDoublyLinkedList (tail);
+                moves = 0; 
+            } 
+        }
+        
+        printf ("%d\n", tail->key);
+        free (tail);
+    }
+    
+    return 0;
+}

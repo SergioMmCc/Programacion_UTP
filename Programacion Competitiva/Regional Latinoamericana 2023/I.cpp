f6862f08e0072fa4de49e89cfd97ef57a7568a09
Sergio Moncada Muñoz <138743635+SergioMmCc@users.noreply.github.com>
2023-11-24 22:23:21 -0500

    Update I.cpp

--- Programacion Competitiva/Regional Latinoamericana 2023/I.cpp
+++ Programacion Competitiva/Regional Latinoamericana 2023/I.cpp
@@ -3,50 +3,48 @@
 #include <math.h>
 #include <string.h>
 #define m 1000000007
-
+ 
 using namespace std;
-
+ 
 int main () {
-    long long int rightFrequencies[30], leftFrequencies[30], left = 0, right = 0, saveLetter, length, result, n;
+    long long int rightFrequencies[30], leftFrequencies[30], left = 0, right = 0, saveLetter, length, result, n, nplus1, nminus1;
     string word;
-    
+ 
     memset (rightFrequencies, 0, sizeof rightFrequencies);
     memset (leftFrequencies, 0, sizeof leftFrequencies);
     cin >> word;
     cin >> n;
+    nplus1 = n + 1;
+    nminus1 = n - 1;
     length = word.size () - 1;
-    
+ 
     for (int letterIndex = 0; letterIndex <= length; letterIndex++) {
         saveLetter = (int)word[letterIndex] - 96;
         for (int frequencyIndex = saveLetter; frequencyIndex < 26; frequencyIndex++)
             leftFrequencies[frequencyIndex]++;
-        
+ 
         left += leftFrequencies[saveLetter - 1];
     }
-    
+ 
     for (int letterIndex = length; letterIndex >= 0; letterIndex--) {
         saveLetter = (int)word[letterIndex] - 96;
         for (int frequencyIndex = saveLetter; frequencyIndex < 26; frequencyIndex++)
             rightFrequencies[frequencyIndex]++;
-        
+ 
         right += rightFrequencies[saveLetter - 1];
     }
-    
-    /*cout << right << "\n";
-    cout << left << "\n";*/
-    
+ 
     if (n % 2 == 0) 
-        result = (((n/2)%m) * (((((right%m)*((n+1)%m))%m) + (((left%m)*((n-1)%m))%m)) %m))%m;
-        //result = (((n/2)%m) * (((right*(n+1)) + (left*(n-1)))%m)) % m;
-    
-    else
-        //result = ((n%m) * ((((right*(n+1)) + (left*(n-1)))/2)%m)) % m;
-        
-        //result = ((n%m) * (((((right * (n+1))%m) + ((left*(n-1)) %m)) %m) /2)) %m; //Esta buena hasta aca
-        
-        result = ((n%m) * ((((((right%m) * ((n+1)%m))%m) + (((left%m) * ((n-1)%m)) %m)) %m) /2)) %m;
+        n /= 2;
+ 
+    else {
+    	nplus1 /= 2;
+    	nminus1 /= 2;
+	}
+ 
+    result = ((n%m) * (((right%m)*(nplus1%m)%m) + ((left%m) * (nminus1%m)%m)%m)) %m;    
 
     cout << result << "\n";
-
+ 
     return 0;
 }
